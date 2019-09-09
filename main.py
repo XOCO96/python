667fee8c6f83d6483d963088e88d17963ead9920
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-09 10:37:18 -0500

    main -> agregadas funciones para ver la lista, agregar un cliente y mostrar la lista de nuevo

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 
-clientes = 'pablo, ricardo,'
+clients = 'pablo, ricardo, '
+
+
+def create_client(client_name):
+    global clients             #global permite que la funcion haga manejo de la variable clients
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients +=', '
+
 
 if __name__ == '__main__':       #punto de partida (la condicion if para que se ejecute el programa)
-    clientes += ' david'
-    print(clientes)
+    list_clients()
+
+    create_client('eduardo')
+
+    list_clients()
