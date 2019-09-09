e8dfdb8ebfbd139a3d00dbefd7a263d67f543bdc
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-09 13:35:50 -0500

    main -> agregado mensaje de bienvenida, funcion para preguntar al usuario si desea agregar o eliminar cliente

--- main.py
+++ main.py
@@ -20,10 +20,24 @@ def _add_comma():
 
     clients +=', '
 
+def _print_welcome():
+    print('Welcome to platzi ventas')
+    print('*'*50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
 
 if __name__ == '__main__':       #punto de partida (la condicion if para que se ejecute el programa)
-    list_clients()
+    _print_welcome()
 
-    create_client('eduardo')
+    command = input()
 
-    list_clients()
+    if command == 'C':
+        client_name = input('what is the client name?')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass                            #placeolder
+    else:
+        print('Invalid Command')
