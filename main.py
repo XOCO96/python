df0d898822348a0b3ca62bc7b3c80015f3a73b52
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-10 08:50:08 -0500

    main -> agregadas operaciones con strings y Update para el nombre de clientes, restructurado la linea que pide el nombre del cliente en una funcion

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', update_client_name + ',')
+    else:
+        print('Client is nor in clients list')
+
+
 def _add_comma():
     global clients
 
@@ -28,19 +37,29 @@ def _print_welcome():
     print('*'*50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+def _get_client_name():
+    return input('What is the client name? ')
 
 
 if __name__ == '__main__':       #punto de partida (la condicion if para que se ejecute el programa)
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('what is the client name?')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass                            #placeolder
+        pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name = input('What is de updated client name? ')
+        update_client(client_name,update_client_name)
+        list_clients()                          #placeolder
     else:
         print('Invalid Command')
