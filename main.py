67cd14bc84d34f506df022ed1bac5d487956b050
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-10 09:55:24 -0500

    main -> modificada la funcion get_client con un while para continuar ejecucion hasta recibir un nombre o de terminar la ejecucion si se ingresa exit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 clients = 'pablo, ricardo, '
 
@@ -63,7 +64,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit() 
+
+    return client_name
 
 
 if __name__ == '__main__':       #punto de partida (la condicion if para que se ejecute el programa)
