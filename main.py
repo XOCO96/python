2249929e04d81dc76f2c2fe7c160516ecb5d8b35
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-12 08:40:08 -0500

    agregada funciones para leer y escribir la lista de clientes en un arhivo csv externo

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import sys
+import csv
+import os
 
-clients = [
-    {
-        'name': 'pablo',
-        'company': 'google',
-        'email': 'pablo@google.com',
-        'position': 'software engineer',
-    },
-    {
-        'name': 'ricardo',
-        'company': 'facebook',
-        'email': 'ricardo@facebook.com',
-        'position': 'data engineer',
-    }
-]
+
+#   Aun falta implementar el reto, para la correcta manipulacion de los diccionarios con los comandos
+#   de U, D y S. 
+
+
+CLIENT_TABLE = '.clients.csv'
+CLIENTS_SCHEMA = ['name','company', 'email', 'position']
+clients = []
+
+
+def _initializa_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:                              #Context managers
+        reader = csv.DictReader(f, fieldnames=CLIENTS_SCHEMA)            #fieldnames es una lista con las llaves que dicteader utilizara para
+                                                                         #construir los diccionarios
+        for row in reader:                                               #Row (filas) representan a cada cliente
+            clients.append(row)
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames = CLIENTS_SCHEMA)
+        writer.writerows(clients)
+
+        os.remove(CLIENT_TABLE)
+        os.rename(tmp_table_name, CLIENT_TABLE)
 
 
 def create_client(client):
-    global clients             #global permite que la funcion haga manejo de la variable clients
+    global clients                                                       #global permite que la funcion haga manejo de la variable clients
 
     if client not in clients:
         clients.append(client)
@@ -96,6 +109,8 @@ def _get_client_name():
 
 
 if __name__ == '__main__':       #punto de partida (la condicion if para que se ejecute el programa)
+    _initializa_clients_from_storage()
+
     _print_welcome()
 
     command = input()
@@ -106,21 +121,21 @@ if __name__ == '__main__':       #punto de partida (la condicion if para que se 
             'name': _get_client_field('name'),
             'company': _get_client_field('company'),
             'email': _get_client_field('email'),
-            'psotion': _get_client_field('position'),
+            'position': _get_client_field('position'),
         }
         create_client(client)
-        list_clients()
+        
     elif command == 'L':
         list_clients()
     elif command == 'D':
         client_name = _get_client_name()
         delete_client(client_name)
-        list_clients()
+        
     elif command == 'U':
         client_name = _get_client_name()
         update_client_name = input('What is de updated client name? ')
         update_client(client_name,update_client_name)
-        list_clients()  
+          
     elif command == 'S':
         client_name = _get_client_name()
         found = search_client(client_name)
@@ -131,3 +146,5 @@ if __name__ == '__main__':       #punto de partida (la condicion if para que se 
             print('The client: {} is not in our client\'s list'.format(client_name))                        #placeolder
     else:
         print('Invalid Command')
+
+    _save_clients_to_storage()
