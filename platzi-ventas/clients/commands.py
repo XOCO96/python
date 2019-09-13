efa5e3b9019e08c19ca058cbf56e852abfa8d4fe
Eduardo Xocoyotzin <lalo_1010_@hotmail.com>
2019-09-13 08:38:22 -0500

    Creado proyecto platzi-ventas reestructurando el codigo empleado al inicio y completandolo

---
+++ platzi-ventas/clients/commands.py
@@ -0,0 +1,57 @@
+import click
+
+from clients.services import ClientService
+from clients.models import ClientModel
+
+@click.group()
+def clients():
+    """Manages the clients lifecycle"""
+    pass
+
+
+@clients.command()
+@click.option('-n', '--name',
+                type=str,
+                prompt=True,
+                help= 'The clients name')
+@click.option('-c', '--company',
+                type=str,
+                prompt=True,
+                help= 'The clients name')
+@click.option('-e', '--email',
+                type=str,
+                prompt=True,
+                help= 'The clients name')
+@click.option('-p', '--position',
+                type=str,
+                prompt=True,
+                help= 'The clients name')
+@click.pass_context
+def create(ctx, name, company, email, position):
+    """creates a new client"""
+    client = client(name,company, email, position)
+    client.services = ClientService(ctx)
+
+
+@click.command()
+@click.pass_context
+def list(ctx):
+    """List all clients"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+    """Updates a client"""
+    pass
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_uid):
+    """delete a client"""
+    pass
+
+
+all = clients
